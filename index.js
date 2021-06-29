d331168d8b6d7920f0889e2482ee840b8c3c564b
Jyothi <jyothiarunkr@gmail.com>
2021-06-29 22:55:29 +0530

    change in index file

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(requestLogger)
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
+    content: "HTML is easy and simple",
     date: "2020-01-10T17:30:31.098Z",
     important: true
   },
